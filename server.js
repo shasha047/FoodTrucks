abea92cdc9bf8b3b9b05792c4e8193d8d432183c
shashank bhatnagar <shasha09047@gmail.com>
2017-05-30 17:36:34 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ var bodyparser = require('body-parser');
 
 // app.set('port',3000);
 app.use(express.static('public'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 app.use(bodyparser.urlencoded({ extended : false }));
 app.use(bodyparser.json());
 
-app.listen(3000,function(){
-//app.listen(process.env.PORT,function(){    
+//app.listen(3000,function(){
+app.listen(process.env.PORT,function(){    
     console.log('Example app listening on port 3000');
 });
